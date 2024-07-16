b84e987cc38156300e458b95303a027bf22659cd
Matt Knighton <matt.knighton@mrkdevelopment.com>
2024-07-16 18:44:18 +1000

    Adjust names inside the breadcrumb filter for the pages inside care plans and features

--- core/Filters/Breadcrumb.php
+++ core/Filters/Breadcrumb.php
@@ -30,26 +30,35 @@ class Breadcrumb extends BaseController {
 	 */
 	public function create_crumb( $crumbs, $class ) {
 
+		do_action( 'qm/debug', $crumbs );
+
 		if ( is_singular( 'cpplugins' ) || is_post_type_archive( 'cpplugins' ) ) {
 
 			$blog_crumb[] = array(
-				'0'              => 'Blog',
+				'0'              => 'Care Plans',
 				'1'              => get_permalink( 25105 ),
 				'hide_in_schema' => '',
 			);
 
 			array_splice( $crumbs, 1, -2, $blog_crumb );
+
+			// Adjust name for breadcrumb.
+			$crumbs[2][0] = 'Care Plan Plugin Support';
 		}
 		if ( is_singular( 'features' ) || is_post_type_archive( 'features' ) ) {
 
 			$blog_crumb[] = array(
-				'0'              => 'Blog',
+				'0'              => 'Care Plans',
 				'1'              => get_permalink( 25105 ),
 				'hide_in_schema' => '',
 			);
 
-			array_unshift( $crumbs, $blog_crumb );
+			array_splice( $crumbs, 1, -2, $blog_crumb );
+
+			// Adjust name for breadcrumb.
+			$crumbs[2][0] = 'Care Plan Feature Matrix';
 		}
+		do_action( 'qm/debug', $crumbs );
 
 		return $crumbs;
 	}
